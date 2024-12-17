6ab8daa5ac3e8170b1d1c76cf8032bcc37b80ddb
Lalit Mohan <111294030+Lalit-mp@users.noreply.github.com>
2024-12-17 16:53:39 +0530

    Add files via upload
    all files uploaded

---
+++ script.js
@@ -0,0 +1,49 @@
+const allButton = document.querySelector(".button-menu");
+const homePageCtn = document.querySelector(".home");
+const profilePageCtn = document.querySelector(".profile");
+const contactPageCtn = document.querySelector(".contact");
+
+    profilePageCtn.classList.add("hide");
+    contactPageCtn.classList.add("hide");
+
+allButton.addEventListener("click", (e) => {
+  console.log(e.target.textContent);
+  if (e.target.textContent === "Home") {
+    allButton.children[0].classList.add("button-bg");
+    if (
+      allButton.children[1].classList.contains("button-bg") ||
+      allButton.children[2].classList.contains("button-bg")
+    ) {
+      allButton.children[1].classList.remove("button-bg");
+      allButton.children[2].classList.remove("button-bg");
+    }
+    profilePageCtn.classList.add("hide");
+        homePageCtn.classList.remove("hide");
+        contactPageCtn.classList.add("hide")
+
+  } else if (e.target.textContent === "Profile") {
+    allButton.children[1].classList.add("button-bg");
+    if (
+      allButton.children[0].classList.contains("button-bg") ||
+      allButton.children[2].classList.contains("button-bg")
+    ) {
+      allButton.children[0].classList.remove("button-bg");
+      allButton.children[2].classList.remove("button-bg");
+    }
+    profilePageCtn.classList.remove("hide");
+        homePageCtn.classList.add("hide");
+        contactPageCtn.classList.add("hide");
+  } else if (e.target.textContent === "Contact") {
+    allButton.children[2].classList.add("button-bg");
+    if (
+      allButton.children[1].classList.contains("button-bg") ||
+      allButton.children[0].classList.contains("button-bg")
+    ) {
+      allButton.children[1].classList.remove("button-bg");
+      allButton.children[0].classList.remove("button-bg");
+    }
+    contactPageCtn.classList.remove("hide");
+        profilePageCtn.classList.add("hide");
+        homePageCtn.classList.add("hide");
+  }
+});
